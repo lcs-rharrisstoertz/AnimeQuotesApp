8c8fe304bc93d2ae7b3afa8f122920063a09730e
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-10-07 09:01:31 -0400

    Initial Commit

---
+++ AnimeQuotesApp/AnimeQuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeQuotesAppApp.swift
+//  AnimeQuotesApp
+//
+//  Created by Harris-Stoertz, Rowan on 2021-10-07.
+//
+
+import SwiftUI
+
+@main
+struct AnimeQuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
