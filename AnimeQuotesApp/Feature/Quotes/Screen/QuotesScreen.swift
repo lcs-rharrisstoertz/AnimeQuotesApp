72ae870dbda3d44ae86cd33fd15dd44683621aa1
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-10-07 09:41:14 -0400

    used abstraction by separating the quote screen and having it call a view from another source

--- AnimeQuotesApp/Feature/Quotes/Screen/QuotesScreen.swift
+++ AnimeQuotesApp/Feature/Quotes/Screen/QuotesScreen.swift
@@ -12,34 +12,9 @@ struct QuotesScreen: View {
         
         List {
             ForEach(Quote.dummyData, id: \.anime) {item in
-                VStack(alignment: .leading, spacing: 8) {
-                    HStack {
-                        Image(systemName: "tv")
-                            .font(.system(size: 12, weight: .black))
-                        Text(item.anime)
-                    }
-                    
-                    Text(makeAttributedString(title: "Character", label: item.character))
-                    Text(makeAttributedString(title: "Quotes", label: item.quote))
-                        .lineLimit(2)
-                }
-                .padding()
-                .foregroundColor(.black)
+                QuoteView(item: item)
             }
         }
-    }
-    
-    private func makeAttributedString(title: String, label: String) -> AttributedString {
-        var string = AttributedString("\(title): \(label)")
-        string.foregroundColor = .black
-        string.font = .system(size: 16, weight: .bold)
-        
-        if let range = string.range(of: label) {
-            string[range].foregroundColor = .black.opacity(0.8)
-            string[range].font = .system(size: 16, weight: .regular)
-            
-        }
-        return string
     }
 }
 
